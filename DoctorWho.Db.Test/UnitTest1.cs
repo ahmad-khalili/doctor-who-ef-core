164932b3b3cd6cf47f2639fd8dde1176757f3b45
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-01 15:37:51 +0300

    Define project structure

---
+++ DoctorWho.Db.Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace DoctorWho.Db.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DoctorWho.Db;
+
+public class DoctorWhoCoreDbContext : DbContext
+{
+    
+}
