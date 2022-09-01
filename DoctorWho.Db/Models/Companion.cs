eaa56365f99fd5ff8d559a7a70c72d8c83ff3691
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-01 16:40:15 +0300

    Create data models & define relationships

---
+++ DoctorWho.Db/Models/Companion.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DoctorWho.Db.Models;
+[Table(nameof(Companion))]
+public class Companion
+{
+    public Companion()
+    {
+        Episodes = new List<Episode>();
+    }
+    
+    public int CompanionId { get; set; }
+    public string CompanionName { get; set; }
+    public string WhoPlayed { get; set; }
+
+    public List<Episode> Episodes { get; set; }
+}
